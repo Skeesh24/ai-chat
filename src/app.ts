400ed71fbb505577fa07e23d776d6c19da80e719
skeesh24 <d.pacooloff@gmail.com>
2023-10-19 18:53:40 +0500

    add morgan lib

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import { config } from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
+import appRouter from './routes/index.js';
 
 config();
 const app = express();
 
+// ====== middlewares ======
+
 app.use(express.json());
+
+// TODO: remove in production
+app.use(morgan('dev'));
+
+app.use("api/v1", appRouter);
 
 export default app;
